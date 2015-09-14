e26992383e03cfa24ff927689e6fd9657a6e1b75
Ben (work) <bscorwin@gmail.com>
2015-09-14 18:37:24 -0500

    Added way to test sms replying Added a page to accept posts from Twilio and to display the parameters it uses.

--- Factz/views.py
+++ Factz/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import twilio.twiml
+
 
 # Create your views here.
+def sms_reply(request):
+    out = "<html>"
+    dict = request.REQUEST
+    for a in request.REQUEST:
+        out += a + ":"
+        out += dict[a] + "<br>"
+    out += "</html>"
+    return HttpResponse(out)
