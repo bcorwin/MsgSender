67966aff6b283bc38e8038b8ab5073cc391456ab
Ben (work) <bscorwin@gmail.com>
2015-09-15 13:22:43 -0500

    Twiml attempt Using @csrf_exempt

--- Factz/views.py
+++ Factz/views.py
@@ -1,12 +1,14 @@
 from Factz.do import format_number
 #from django_twilio.decorators import twilio_view
-from twilio.twiml import Response
+#from twilio.twiml import Response
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+#@twilio_view
 @csrf_exempt
 def sms_reply(request):
     #str(request.REQUEST)
-    r = Response()
-    r.message('Hello from your Django app!')
-    return HttpResponse(r.toxml(), content_type='text/xml')
+    #r = Response()
+    #r.message('Hello from your Django app!')
+    twiml = '<Response><Message>Hello from your Django app!</Message></Response>'
+    return HttpResponse(twiml, content_type='text/xml')
