4a544a7d6ca6bba56ff2ea03d0bfd8b8d2a5f92b
Ben (work) <bscorwin@gmail.com>
2015-09-14 20:12:25 -0500

    Twimil test Replying with request info to see what's there and to test replying.

--- Factz/views.py
+++ Factz/views.py
@@ -6,19 +6,6 @@ import twilio.twiml
 
 # Create your views here.
 def sms_reply(request):
-    out = "<html>"
-    dict = request.REQUEST
-    for a in request.REQUEST:
-        out += a + ":"
-        out += dict[a] + "<br>"
-        
-    try:
-        from_number = dict["From"]
-        from_number = format_number(from_number)
-        cc = Number.objects.get(phone_number=from_number).phone_number
-        out += "<br>Your confirmation code: " + cc
-    except:
-        out += "<br>Number is not valid."
-    finally:
-        out += "</html>"
-    return HttpResponse(out)
+    resp = twilio.twiml.Response()
+    resp.message(str(request.REQUEST))
+    return HttpResponse(resp.toxml(), content_type="text/xml")
