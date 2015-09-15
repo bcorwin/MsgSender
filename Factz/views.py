f08e208c8c9c33736d04dc9353f184eceade0c13
Ben (work) <bscorwin@gmail.com>
2015-09-15 15:03:20 -0500

    Changed reply message

--- Factz/views.py
+++ Factz/views.py
@@ -1,12 +1,16 @@
 from Factz.do import format_number
 #from django_twilio.decorators import twilio_view
-#from twilio.twiml import Response
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from Factz.models import Number
 
 #@twilio_view
 @csrf_exempt
 def sms_reply(request):
-    from_number = request.POST['From']
-    twiml = '<Response><Message>Your number:' + from_number + '</Message></Response>'
+    from_number = format_number(request.POST['From'])
+    try:
+        cc = Number.objects.get(phone_number=from_number).confirmation_code
+        twiml = '<Response><Message>Your confirmation code:' + cc + '</Message></Response>'
+    except:
+        twiml = '<Response><Message>To sign up for PoopFactz go to www.PoopFactz.com</Message></Response>'
     return HttpResponse(twiml, content_type='text/xml')
