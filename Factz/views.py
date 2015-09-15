6c90641d83b48e461e8b87ca9d5608342cf62a07
Ben (work) <bscorwin@gmail.com>
2015-09-15 14:40:51 -0500

    Sending back number test. Not using twiml code (can't get it to work)

--- Factz/views.py
+++ Factz/views.py
@@ -1,14 +1,15 @@
 from Factz.do import format_number
 #from django_twilio.decorators import twilio_view
-from twilio.twiml import Response
+#from twilio.twiml import Response
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from Factz.models import Number
 
 #@twilio_view
-@csrf_exempt
+
 def sms_reply(request):
-    from_number = format_number(request.POST["From"])
-    cc = Number.objects.get(phone_number=from_number).confirmation_code
-    r = Response()
-    r.message('Your confirmation: ' + cc)
-    return r
+    #str(request.REQUEST)
+    #r = Response()
+    #r.message('Hello from your Django app!')
+    from_number = request.REQUEST['From']
+    twiml = '<Response><Message>Your number:' + from_number + '</Message></Response>'
+    return HttpResponse(twiml, content_type='text/xml')
