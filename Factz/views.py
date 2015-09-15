6e47ccbc39af88f531eeead3aae40252a35e3254
Ben (work) <bscorwin@gmail.com>
2015-09-15 14:51:11 -0500

    Testing twilio.twiml.Response()

--- Factz/views.py
+++ Factz/views.py
@@ -1,15 +1,13 @@
 from Factz.do import format_number
 #from django_twilio.decorators import twilio_view
-#from twilio.twiml import Response
-from django.http import HttpResponse
+from twilio.twiml import Response
+#from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 #@twilio_view
 @csrf_exempt
 def sms_reply(request):
-    #str(request.REQUEST)
-    #r = Response()
-    #r.message('Hello from your Django app!')
-    from_number = request.REQUEST['From']
-    twiml = '<Response><Message>Your number:' + from_number + '</Message></Response>'
-    return HttpResponse(twiml, content_type='text/xml')
+    from_number = request.POST['From']
+    r = Response()
+    r.message('Hello from your Django app!')
+    return r
