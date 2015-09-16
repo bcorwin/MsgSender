fd8674a3f2bd386a1005a316862725c121fbdb3d
Ben (work) <bscorwin@gmail.com>
2015-09-16 11:59:39 -0500

    Added activeSubscription to admin page

--- Factz/admin.py
+++ Factz/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from Factz.models import Variable, Message, Subscription, Number
+from Factz.models import Variable, Message, Subscription, Number, activeSubscription
 
 admin.site.register(Message)
 admin.site.register(Number)
 admin.site.register(Subscription)
 admin.site.register(Variable)
+admin.site.register(activeSubscription)
