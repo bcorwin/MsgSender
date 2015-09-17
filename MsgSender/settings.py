42e52e7aa4040bc3b2e01814c00a86940274f9f5
Benjamin Scott Corwin <bscorwin@gmail.com>
2015-09-16 19:40:30 -0500

    Unrelated changes
    -Added __str__ to activeSubscriptions
    -Corrected local issue with python paths
    -Added static folder to hopefully help with admin working in prod

--- MsgSender/settings.py
+++ MsgSender/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, sys
+sys.path.append("C:\Anaconda\Lib\site-packages")
 import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
