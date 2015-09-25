19ef0aadf8256967062ae770b7468e738749a34a
Benjamin Scott Corwin <bscorwin@gmail.com>
2015-09-24 21:43:29 -0500

    Modified the way models are being displayed in admin

--- Factz/admin.py
+++ Factz/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from Factz.models import Variable, Message, Subscription, Number, activeSubscription
 
-admin.site.register(Message)
-admin.site.register(Number)
-admin.site.register(Subscription)
-admin.site.register(Variable)
-admin.site.register(activeSubscription)
+def set_active(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+def set_inactive(modeladmin, request, queryset):
+    queryset.update(active=False)
+
+class activeSubAdmin(admin.ModelAdmin):
+    list_display = ['number', 'subscription', 'active']
+    actions = [set_active, set_inactive]
+    
+class messageAdmin(admin.ModelAdmin):    
+    list_display = ['subscription', 'message', 'follow_up', 'last_sent', 'active']
+    actions = [set_active, set_inactive]
+    
+class numberAdmin(admin.ModelAdmin):
+    list_display = ['phone_number', 'last_sent']
+
+class subAdmin(admin.ModelAdmin):
+    list_display = ['name', 'last_sent']
+    
+class varAdmin(admin.ModelAdmin):
+    list_display = ['name', 'val']
+
+admin.site.register(Message, messageAdmin)
+admin.site.register(Number, numberAdmin)
+admin.site.register(Subscription, subAdmin)
+admin.site.register(Variable, varAdmin)
+admin.site.register(activeSubscription, activeSubAdmin)
