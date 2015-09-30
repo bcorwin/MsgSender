70127a4a616ca9f0c4a08bda8dc1131c07d99b5f
Ben (work) <bscorwin@gmail.com>
2015-09-30 12:02:30 -0500

    Merged migrations

---
+++ Factz/migrations/0028_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Factz', '0027_auto_20150930_0937'),
+        ('Factz', '0027_auto_20150930_1029'),
+    ]
+
+    operations = [
+    ]
