dd33371b8f00a3b26191c22fc763a20e2418bf0c
Benjamin Scott Corwin <bscorwin@Gmail.com>
2015-10-13 22:28:26 -0500

    Another attempt

--- Factz/migrations/0042_auto_20151013_2018.py
+++ Factz/migrations/0042_auto_20151013_2018.py
@@ -3,14 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-ALTER_SQL = '''
-    ALTER TABLE Factz.sentMessage ALTER COLUMN attempted TYPE integer USING (
-        CASE attempted
-            when TRUE then 1
-            when FALSE then 0
-        END
-        );
-    '''
+ALTER TABLE Factz.sentMessage ALTER COLUMN attempted TYPE integer USING (CASE attempted when TRUE then 1 when FALSE then 0 END);
 
 class Migration(migrations.Migration):
 
