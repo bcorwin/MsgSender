e103d51b7bbf0aefe14837f5d96a35e5d0c387e8
Benjamin Scott Corwin <bscorwin@gmail.com>
2015-10-13 22:37:25 -0500

    Reverting attempted fix

--- Factz/migrations/0042_auto_20151013_2018.py
+++ Factz/migrations/0042_auto_20151013_2018.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-ALTER TABLE Factz.sentMessage ALTER COLUMN attempted TYPE integer USING (CASE attempted when TRUE then 1 when FALSE then 0 END);
-
 class Migration(migrations.Migration):
 
     dependencies = [
