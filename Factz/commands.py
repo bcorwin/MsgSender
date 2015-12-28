304529105ff525e715d3127021e2a3f642b680d6
Ben (work) <bscorwin@gmail.com>
2015-12-28 11:12:59 -0600

    Removed add as a possible regex for subscribe

--- Factz/commands.py
+++ Factz/commands.py
@@ -11,7 +11,7 @@ class textCommand:
         return self.display_name
         
 commands = [
-            textCommand("subscribe",    "Subscribe PoopFactz",  ["subscribe *(.*)", "add *(.*)"]),
+            textCommand("subscribe",    "Subscribe PoopFactz",  ["subscribe *(.*)"]),
             textCommand("unsubscribe",  "Unsubscribe PoopFactz",["unsubscribe *(.*)", "leave *(.*)", "stop *(.*)"]),
             textCommand("source",       "Source",               ["source()", "sauce()"]),
             textCommand("rate",         "Rate #",               ["rate *(\d*)", "rating *(\d*)", "(\d+)"]),
